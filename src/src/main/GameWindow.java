efefb4b199b4483dd4b85c15e6bc694f39e9acd0
Trần Ngọc Đăng Khôi <khoitranngocdg@gmail.com>
2023-03-07 00:37:48 +0700

    Rename some stuffs
    I fix the name of reso  -> res folder to unify as same as tutorial, so fetch it again before coding

--- src/src/main/GameWindow.java
+++ src/src/main/GameWindow.java
@@ -11,7 +11,7 @@ public class GameWindow {
         jframe.setSize(400,400);                    //set size của cửa sổ
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //set up đóng hoàn toàn
         jframe.add(gamePanel);                                   //add cái đã draw ở gamePanel qua.
-        jframe.setLocationRelativeTo(null); //set screen at the center
+        jframe.setLocationRelativeTo(null);                     //set screen at the center
         jframe.setResizable(false);                             //unallow to resize the window
         jframe.pack();                                          //fit windows
         jframe.setVisible(true);                                //set hiện khung cửa sổ
